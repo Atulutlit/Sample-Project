09effc4c0a988062084f3dce14dbe580364240ec
Ashutosh Singh <ashutoshsingh202003@gmail.com>
2026-04-19 08:42:54 +0530

    changes made

--- src/component/C.tsx
+++ src/component/C.tsx
@@ -1,11 +1,11 @@
-
-const C = ({count, setCount}:any) => {
+const C = ({ count, setCount }: any) => {
   return (
     <>
+      <h1>this is going to be the heading</h1>
       <div>{count}</div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
-  )
-}
+  );
+};
 
-export default C
+export default C;
